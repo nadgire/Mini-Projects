3c118294fbefefb24ae8fb190f80377f37da6774
nadgire <abhinadgire@hotmail.com>
2024-12-27 12:06:34 +0530

    Fetching States

--- SignUpForm/index.js
+++ SignUpForm/index.js
@@ -16,12 +16,27 @@ connectObj.onreadystatechange = function () {
 }
 
 function fillDataListCountriesWithCode(countries) {
-    for (let i = 0; i < countries.length; i++){
-        selectCountry.innerHTML+="<option>"+countries[i].dial_code+" "+countries[i].name+"</option>";
+    for (let i = 0; i < countries.length; i++) {
+        selectCountry.innerHTML += "<option>" + countries[i].dial_code + " " + countries[i].name + "</option>";
     }
     phoneCountryCode.addEventListener("input", loadStates);
 }
 
-function loadStates(event){
-    console.log(event.target.value);
+function loadStates(event) {
+    // console.log(event.target.value);
+    var codeAndCountry = event.target.value.split(" ");
+    // console.log(codeAndCountry);
+
+    connectObj.open("get", "https://countriesnow.space/api/v0.1/"+codeAndCountry[1]+"/states", true);
+    connectObj.send();
+
+    connectObj.onreadystatechange = function () {
+        if (connectObj.readyState == 4 && connectObj.status == 200) {
+            var response = connectObj.responseText;
+            // console.log(response);
+            var result = JSON.parse(response);
+            // console.log(result.data);
+            fillDataListCountriesWithCode(result.data);
+        }
+    }
 }
